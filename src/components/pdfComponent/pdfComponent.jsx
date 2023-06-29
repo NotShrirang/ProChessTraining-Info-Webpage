57c4694ecb5dfa455fdd66e789fe991183b0b978
notshrirang <shrirangmahajan123@gmail.com>
2023-06-29 16:50:20 +0530

    chore: change schedules

--- src/components/pdfComponent/pdfComponent.jsx
+++ src/components/pdfComponent/pdfComponent.jsx
@@ -8,9 +8,7 @@ const PdfComponent = ({ group }) => {
   // const price = { 1: "INR 4300", 2: "INR 5000", 3: "INR 5750", 5: "INR 18250" }[
   //   group
   // ];
-  const price = { 1: "$60", 2: "$70", 3: "$80", 5: "$250" }[
-    group
-  ];
+  const price = { 1: "$60", 2: "$70", 3: "$80", 5: "$250" }[group];
 
   if (group == "0") {
     return <></>;
@@ -25,10 +23,10 @@ const PdfComponent = ({ group }) => {
       </div>
       <div className="body-schedule">Schedule</div>
       <div className="body-pdf">
-        <a href={`/Group${group}_June2023.pdf`}>
+        <a href={`/Group${group}_July2023.pdf`}>
           <img
-            src={`/Group${group}_June2023.png`}
-            alt={`/Group${group} - June 2023 Schedule`}
+            src={`/Group${group}_July2023.png`}
+            alt={`/Group${group} - July 2023 Schedule`}
             width="100%"
             height="100%"
           />
